4f89785d97c95ee911238c44ac7ab2a1eaa28c80
kana <kozakana333@gmail.com>
2018-05-26 13:29:02 +0900

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import Flask, request, abort
+from flask import Flask, request, abort, send_file
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -9,8 +9,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, LocationMessage, TextSendMessage, ImageSendMessage
+    MessageEvent, TextMessage, LocationMessage, 
+    LocationSendMessage,TextSendMessage, StickerSendMessage, 
+    MessageImagemapAction, ImagemapArea, ImagemapSendMessage, BaseSize
 )
+
+from io import BytesIO, StringIO
+from PIL import Image
+import requests
+import urllib.parse
 
 app = Flask(__name__)
 
@@ -27,6 +34,8 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
+
+
 
 @app.route("/")
 def hello_world():
@@ -62,6 +71,17 @@ def handle_message(event):
             ]
         )
 
+@app.route("/imagemap/<path:url>/<size>")
+def imagemap(url, size):
+    map_image_url = urllib.parse.unquote(url)
+    response = requests.get(map_image_url)
+    img = Image.open(BytesIO(response.content))
+    img_resize = img.resize((int(size), int(size)))
+    byte_io = BytesIO()
+    img_resize.save(byte_io, 'PNG')
+    byte_io.seek(0)
+    return send_file(byte_io, mimetype='image/png')
+
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location(event):
@@ -75,13 +95,29 @@ def handle_location(event):
     key = 'AIzaSyD_0kx_crEIA5mMLJWnfZN9Fo86Odp4LGY'
 
     map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom={}&size=520x520&scale=2&maptype=roadmap&key={}'.format(lat, lon, zoomlevel, key)
+
+    actions = [
+        MessageImagemapAction(
+            text = 'テスト',
+            area = ImagemapArea(
+                x = 0,
+                y = 0,
+                width = 1040,
+                height = 1040
+        )
+    )]
+
+
     line_bot_api.reply_message(
         event.reply_token,
         [
             TextSendMessage(text="位置情報"),
-            TextSendMessage(text='{}\n{}\n{}'.format(event.message.address,event.message.latitude, event.message.longitude)),
-            ImageSendMessage(original_content_url=map_image_url, preview_image_url=map_image_url)
-            
+            ImagemapSendMessage(
+                base_url = 'https://{}/imagemap/{}'.format(request.host, urllib.parse.quote_plus(map_image_url)),
+                alt_text = '地図',
+                base_size = BaseSize(height=imagesize, width=imagesize),
+                actions = actions
+            )
         ]
     )
 
