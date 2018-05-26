127619a17b4703e4916d3948955478331f14bd74
kana <kozakana333@gmail.com>
2018-05-26 13:47:08 +0900

    app.py

--- app.py
+++ app.py
@@ -71,54 +71,6 @@ def handle_message(event):
             ]
         )
 
-@app.route("/imagemap/<path:url>/<size>")
-def imagemap(url, size):
-    map_image_url = urllib.parse.unquote(url)
-    response = requests.get(map_image_url)
-    img = Image.open(BytesIO(response.content))
-    img_resize = img.resize((int(size), int(size)))
-    byte_io = BytesIO()
-    img_resize.save(byte_io, 'PNG')
-    byte_io.seek(0)
-    return send_file(byte_io, mimetype='image/png')
-
-
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location(event):
-
-    lat = event.message.latitude
-    lon = event.message.longitude
-
-    zoomlevel = 16
-    imagesize = 1040
-
-    key = 'AIzaSyD_0kx_crEIA5mMLJWnfZN9Fo86Odp4LGY'
-
-    map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom={}&size=520x520&scale=2&maptype=roadmap&key={}'.format(lat, lon, zoomlevel, key)
-
-    actions = [
-        MessageImagemapAction(
-            text = 'テスト',
-            area = ImagemapArea(
-                x = 0,
-                y = 0,
-                width = 1040,
-                height = 1040
-        )
-    )]
-
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        [
-            ImagemapSendMessage(
-                base_url = 'https://{}/imagemap/{}'.format(request.host, urllib.parse.quote_plus(map_image_url)),
-                alt_text = '地図',
-                base_size = BaseSize(height=imagesize, width=imagesize),
-                actions = actions
-            )
-        ]
-    )
 
 
 if __name__ == "__main__":
