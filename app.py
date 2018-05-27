830c4337789b5c2cfa7c4223248bbed18e4951de
kana <kozakana333@gmail.com>
2018-05-27 21:40:39 +0900

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,17 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, LocationMessage, TextSendMessage, ImageSendMessage, ImagemapSendMessage, BaseSize, MessageImagemapAction, ImagemapArea
+    MessageEvent, 
+    TextMessage, 
+    LocationMessage, 
+    LocationSendMessage,
+    TextSendMessage, 
+    ImageSendMessage,
+    StickerSendMessage, 
+    MessageImagemapAction, 
+    ImagemapArea, 
+    ImagemapSendMessage, 
+    BaseSize
 )
 
 from io import BytesIO, StringIO
@@ -33,6 +43,8 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+
+
 @app.route("/")
 def hello_world():
     return "hello world!"
@@ -55,20 +67,6 @@ def callback():
     return 'OK'
 
 
-
-# @app.route("/imagemap/<path:url>/<size>")
-# def imagemap(url, size):
-    
-#     map_image_url = urllib.parse.unquote(url)
-#     response = requests.get(map_image_url)
-#     img = Image.open(BytesIO(response.content))
-#     img_resize = img.resize((int(size), int(size)))
-#     byte_io = BytesIO()
-#     img_resize.save(byte_io, 'PNG')
-#     byte_io.seek(0)
-#     return send_file(byte_io, mimetype='image/png')
-
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.type == "message":
@@ -81,9 +79,58 @@ def handle_message(event):
             ]
         )
 
+# @app.route("/imagemap/<path:url>/<size>")
+# def imagemap(url, size):
+#     map_image_url = urllib.parse.unquote(url)
+#     response = requests.get(map_image_url)
+#     img = Image.open(BytesIO(response.content))
+#     img_resize = img.resize((int(size), int(size)))
+#     byte_io = BytesIO()
+#     img_resize.save(byte_io, 'PNG')
+#     byte_io.seek(0)
+#     return send_file(byte_io, mimetype='image/png')
 
 
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location(event):
 
+    lat = event.message.latitude
+    lon = event.message.longitude
+
+    zoomlevel = 16
+    imagesize = 1040
+
+    key = 'AIzaSyD_0kx_crEIA5mMLJWnfZN9Fo86Odp4LGY'
+
+    map_image_url = 'https://maps.googleapis.com/maps/api/staticmap?center={},{}&zoom={}&size=520x520&scale=2&maptype=roadmap&key={}'.format(lat, lon, zoomlevel, key)
+
+    # actions = [
+    #     MessageImagemapAction(
+    #         text = 'テスト',
+    #         area = ImagemapArea(
+    #             x = 0,
+    #             y = 0,
+    #             width = 1040,
+    #             height = 1040
+    #     )
+    # )]
+
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        [
+            TextSendMessage(text="位置情報"),
+            ImageSendMessage(original_content_url=map_image_url,preview_image_url=map_image_url)
+                # base_url = 'https://{}/imagemap/{}'.format(request.host, urllib.parse.quote_plus(map_image_url)),
+                # alt_text = '地図',
+                # base_size = BaseSize(height=imagesize, width=imagesize),
+                # actions = actions
+                
+            
+            
+        ]
+    )
 
 if __name__ == "__main__":
     app.run()
+
