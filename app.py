06c55465dc699d925d50f471ce1ecebf7b851e33
kana <kozakana333@gmail.com>
2018-05-31 20:02:49 +0900

    postgresql

--- app.py
+++ app.py
@@ -67,14 +67,13 @@ def handle_message(event):
     if event.message.text.isdigit():
         user_id = event.source.user_id
         print(user_id)
-        # conn = psycopg2.connect("dbname=" + dbname + " host=" + host + " user=" + user + " password=" + password)
-        # cur = conn.cursor()
-        # cur.execute("SELECT pins FROM users WHERE user_id = 'U14146d611c19d261d47a167d0cadf0d6' ")
-        # row = cur.fetchone()
-        # print(row)
-        # pins = list(row)
-
-        # conn.commit()
+        conn = psycopg2.connect("dbname=" + dbname + " host=" + host + " user=" + user + " password=" + password)
+        cur = conn.cursor()
+        loc = cur.execute("SELECT pins FROM users WHERE user_id = 'U14146d611c19d261d47a167d0cadf0d6' ")
+        row = cur.fetchone()
+        print(row)
+        print(loc)        
+        conn.commit()
 
         line_bot_api.reply_message(
             event.reply_token,
